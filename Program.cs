7960417fa56af5c0f21e52085dc6f16c04c03744
SaiwenU <Kevin.zhu2@student.kpu.ca>
2024-09-13 11:43:12 -0700

    Lab one
    Lab one

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace GD12_1133_Lab1_Kevin_Zhu
+﻿using System;
+
+namespace GD12_1133_Lab1_Kevin_Zhu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi, Im Kevin ,today is 2024-09-13,I'm showing the lab one");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Binary Number");
+            Console.WriteLine("0,01,10,100,1000,10000,100000,1000000,1100100,11111111");
+            Console.WriteLine("Hex Numbers base 16");
+            Console.WriteLine("0,1,2,4,8,10,20,40,64,ff");
+            Console.WriteLine("Decimals Number");
+            Console.WriteLine("0,1,2,4,8,16,32,64,100,255");
+            Console.WriteLine("Thank U,and nice to see U");
         }
     }
 }
