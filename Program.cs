a5e2226464f078b2ef91220e702f10dee2fcf445
SaiwenU <Kevin.zhu2@student.kpu.ca>
2024-09-13 12:52:58 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace GD12_1133_Lab1_Kevin_Zhu
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, Im Kevin ,today is 2024-09-13,I'm showing the lab one");
-            Console.WriteLine("Hello, World!");
             Console.WriteLine("Binary Number");
             Console.WriteLine("0,01,10,100,1000,10000,100000,1000000,1100100,11111111");
             Console.WriteLine("Hex Numbers base 16");
